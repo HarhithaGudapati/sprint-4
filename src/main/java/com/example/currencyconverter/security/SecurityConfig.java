f873bd9ceec52f5bdbc0d8f3bdea207cc2f0957c
HarshithaGudapati <gudapatiharshitha8247@gmail.com>
2024-12-10 00:01:35 -0500

    Update SecurityConfig.java

--- src/main/java/com/example/currencyconverter/security/SecurityConfig.java
+++ src/main/java/com/example/currencyconverter/security/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.example.currencyconverter.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,7 +10,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 public class SecurityConfig {
 
