e283df023ceb0475484e4c32bf1464fc50b46759
HarshithaGudapati <gudapatiharshitha8247@gmail.com>
2024-12-10 00:00:51 -0500

    Update FavoriteController.java

--- src/main/java/com/example/currencyconverter/controller/FavoriteController.java
+++ src/main/java/com/example/currencyconverter/controller/FavoriteController.java
@@ -1,12 +1,10 @@
 package com.example.currencyconverter.controller;
-
 import com.example.currencyconverter.service.CurrencyConverterService;
 import com.example.currencyconverter.service.ConversionHistory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 @Controller
 @RequestMapping("/favorites")
 public class FavoriteController {
